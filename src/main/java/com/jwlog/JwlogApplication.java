99f556e4d9ece4c497b77cc770354b22b847b147
shin.jewoo <shinjw0926@naver.com>
2023-06-12 23:08:49 +0900

    프로젝트 생성

---
+++ src/main/java/com/jwlog/JwlogApplication.java
@@ -0,0 +1,13 @@
+package com.jwlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwlog/controller/PostControllerTest.java
@@ -0,0 +1,29 @@
+package com.jwlog.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("posts 요청시 Hello Jewoo 출력")
+    void test() throws Exception {
+        // expected
+        mockMvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Jewoo"))
+                .andDo(print());
+    }
+}
