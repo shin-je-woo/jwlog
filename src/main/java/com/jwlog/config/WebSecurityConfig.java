bdde1a6a0aa64e44abfdc6535d2a36386978bbfc
shin.jewoo <shinjw0926@naver.com>
2023-07-06 23:18:56 +0900

    스프링 시큐리티 기본 설정

---
+++ src/main/java/com/jwlog/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.jwlog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers("/favicon.ico")
+                .requestMatchers("/error")
+                .requestMatchers(toH2Console());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
--- src/main/java/com/jwlog/controller/AuthController.java
+++ src/main/java/com/jwlog/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.jwlog.request.Signup;
 import com.jwlog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @GetMapping("/auth/login")
+    public String login() {
+        return "로그인 페이지입니다.";
+    }
 
     @PostMapping("/auth/signup")
     public void signup(@RequestBody Signup signup) {
