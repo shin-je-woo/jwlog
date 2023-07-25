ac968983d3bc0bcd0bb467b67c2f783275e6c3d5
shin.jewoo <shinjw0926@naver.com>
2023-07-25 22:41:08 +0900

    DB정보로 로그인

--- src/main/java/com/jwlog/config/WebSecurityConfig.java
+++ src/main/java/com/jwlog/config/WebSecurityConfig.java
@@ -1,17 +1,18 @@
 package com.jwlog.config;
 
+import com.jwlog.domain.User;
+import com.jwlog.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
@@ -31,8 +32,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests()
-                    .requestMatchers("/auth/login").permitAll()
-                    .anyRequest().authenticated()
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/signup").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                     .loginPage("/auth/login")
@@ -45,26 +47,26 @@ public class WebSecurityConfig {
                         .alwaysRemember(false)
                         .tokenValiditySeconds(2592000)
                 )
-                .userDetailsService(userDetailsService())
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        UserDetails user = User
-                .withUsername("shinjw")
-                .password("1234")
-                .roles("ADMIN")
-                .build();
-        manager.createUser(user);
-
-        return manager;
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> {
+            User user = userRepository.findByEmail(username)
+                    .orElseThrow(() -> new UsernameNotFoundException(username + "을 찾을 수 없습니다."));
+            return new UserPrincipal(user);
+        };
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new SCryptPasswordEncoder(
+                16,
+                8,
+                1,
+                32,
+                64);
     }
 }
--- src/main/java/com/jwlog/service/AuthService.java
+++ src/main/java/com/jwlog/service/AuthService.java
@@ -1,12 +1,12 @@
 package com.jwlog.service;
 
-import com.jwlog.crypto.PasswordEncoder;
 import com.jwlog.domain.User;
 import com.jwlog.exception.AlreadyExistsEmailException;
 import com.jwlog.repository.UserRepository;
 import com.jwlog.request.Signup;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,11 +17,13 @@ import java.util.Optional;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void signup(Signup signup) {
         valdateDuplEmail(signup);
 
-        String encryptedPassword = PasswordEncoder.encrypt(signup.getPassword());
+        String encryptedPassword = passwordEncoder.encode(signup.getPassword());
+
         User user = User.builder()
                 .name(signup.getName())
                 .password(encryptedPassword)
